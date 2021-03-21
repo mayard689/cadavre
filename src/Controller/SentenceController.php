e7dcd95c4e9cdfeda223630540fdba29f2c086a1
mayard689 <doc-albert@laposte.net>
2021-03-21 16:57:56 +0100

    change the 'sentence' route to 'contribution'

--- src/Controller/SentenceController.php
+++ src/Controller/SentenceController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/sentence")
+ * @Route("/contribution")
  */
 class SentenceController extends AbstractController
 {
