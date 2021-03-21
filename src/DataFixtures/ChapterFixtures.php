5e21fead76c936083e89499632648a65d623a283
mayard689 <doc-albert@laposte.net>
2021-03-21 09:01:35 +0100

    add fxtures for the chapter entity

---
+++ src/DataFixtures/ChapterFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Chapter;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ChapterFixtures extends Fixture
+{
+    const CHAPTERS = [
+        0 => [
+            'introduction' => 'intro chapitre 1',
+            'conclusion' => 'conclu chapitre 1',
+            'code' => 'code1'
+        ],
+
+        1 => [
+            'introduction' => 'intro chapitre 2',
+            'conclusion' => 'conclu chapitre 2',
+            'code' => 'code2'
+        ],
+
+        2 => [
+            'introduction' => 'intro chapitre 3',
+            'conclusion' => 'conclu chapitre 3',
+            'code' => 'code3'
+        ],
+
+        3 => [
+            'introduction' => 'intro chapitre 4',
+            'conclusion' => 'conclu chapitre 4',
+            'code' => 'code4'
+        ],
+
+        4 => [
+            'introduction' => 'intro chapitre 5',
+            'conclusion' => 'conclu chapitre 5',
+            'code' => 'code5'
+        ],
+    ];
+
+
+    public function load(ObjectManager $manager)
+    {
+        for($i=0; $i<count(self::CHAPTERS); $i++){
+
+            $chapter = new Chapter();
+            $chapter->setIntroduction(self::CHAPTERS[$i]['introduction']);
+            $chapter->setConclusion(self::CHAPTERS[$i]['conclusion']);
+            $chapter->setCode(self::CHAPTERS[$i]['code']);
+
+            $this->addReference('chapter_' .$i, $chapter);
+            $manager->persist($chapter);
+        }
+
+        $manager->flush();
+    }
+}
