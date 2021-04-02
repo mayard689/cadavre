d2d0a2eca99eff8d31fbde8be5fee572330acc43
mayard689 <doc-albert@laposte.net>
2021-04-02 11:52:50 +0200

    update sentence fixtures index

--- src/DataFixtures/SentenceFixtures.php
+++ src/DataFixtures/SentenceFixtures.php
@@ -31,20 +31,20 @@ class SentenceFixtures extends Fixture implements DependentFixtureInterface
             ou bleu, parfois allongés ou arrondis. Le lapin ajoutait d\'une voix déformée en regardant sa montre à gousset 
             "Le moment venu, faites attention à ne pas être en retard". 
             Quand Lewis et Rebecca comprirent ce qui leur arrivait, ils étaient dans une forêt peu banale... comment dire ? Multicolore.',
-            'chapter' => 'chapter_3',
+            'chapter' => 'chapter_2',
         ],
         3 => [
             'text' => 'Rebecca marcha sur un oeuf ! Une oeuf aussi dur devait être fait de béton ! sans se casser, il se 
             mit à rouler et Rebacca de glisser. Apres plusieurs tours sur elle même, elle fini sa cascade dans la rivière. Alice sauta pour aller l\'aider.
             Poissons volants, bulles géantes, algues écarlates et pêcheurs fous, cette rivière fourmillait d\'étrangetés.',
-            'chapter' => 'chapter_4',
+            'chapter' => 'chapter_3',
         ],
         4 => [
             'text' => 'Sortis de la rivière on ne sait trop comment, nos amis fure séché en un rien de temps par des abeilles-seches-cheveux venu leur prêter main forte. 
             Reprenant leur calme peu à peu, Alice, rebecca et le lapin blanc remarquèrent les 3 portes devant eux avec ce petit panneaux 
             "Préférez vous avoir le choix ou l\'embarras ? Peu importe si vous ne trouvez pas la Carotarêve, c\'est elle qui vous cherchera.".
             Rebecca s\'écria alors : "je sais quelle porte utiliser !"',
-            'chapter' => 'chapter_5',
+            'chapter' => 'chapter_4',
         ],
         5 => [
             'text' => 'Reprenant leur chemin, les 3 explorateurs avancent dans la forêt. Le chemin est sinueux et chaque 
@@ -52,7 +52,7 @@ class SentenceFixtures extends Fixture implements DependentFixtureInterface
             des abeilles en as de pique, ou des singes qui se tiennent à carreau". Mais finalement, alors que la foret semblait interminable, tout disparut soudainement.
             Le lapin blanc aussi un sourcil et leur dit "nous y voilà. Nous sommes dans la clairière magique". Un peu plus 
             loin se trouvait un nid et dans le nid, aucun doute, la Carotarêve était là. Bizarre, obscure et sombre.',
-            'chapter' => 'chapter_6',
+            'chapter' => 'chapter_5',
         ],
         6 => [
             'text' => 'Satisfaite de sa découverte tant convoitée mais quand même bien fatiguée, Alice ramassa un champignon 
@@ -61,14 +61,14 @@ class SentenceFixtures extends Fixture implements DependentFixtureInterface
             clairière se mirent à gonfler lentement. La montre du lapin de mit à sonner dans un "dring" qui rappelait le 
             réveil d\'Alice. "Vite, je vous avais prévenu, il est l\'heure de ne pas être en retard. Partons avant que tous 
             ça ne se termine en fondue au chocolat". Alice attrape la Carotarêve et se met à courir.',
-            'chapter' => 'chapter_7',
+            'chapter' => 'chapter_6',
         ],
         7 => [
             'text' => 'C\'est alors que les nuages se mirents à onduler, parfois oranges 
             ou bleus, parfois allongés ou arrondis. Une voix déformée leur dit, "Faites attention à ne pas être en retard". 
             Alice et Rebecca étaient revenues à l\'école. Ne sachant que faire de la Carotarêve, elle décidèrent de la cacher parmi les plantations. 
             Lucy et Honorine passèrent par là sans remarquer la Carotarêve.',
-            'chapter' => 'chapter_8',
+            'chapter' => 'chapter_7',
         ],
     ];
 
