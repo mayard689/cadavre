b84013e68e0a95a48606300b98c4b85afbca5984
mayard689 <doc-albert@laposte.net>
2021-05-24 15:32:06 +0200

    remove useless method in mailSender service

--- src/Service/MailSender.php
+++ src/Service/MailSender.php
@@ -36,7 +36,7 @@ class MailSender
     {
         $email = (new TemplatedEmail())
             ->from($this->params->get("mailer_from"))
-            ->subject('Communication de Villereau');
+            ->subject('Publication du Comité des fêtes de Villereau');
 
         $members = $this->newsletterEmailRepository->findAll();
 
@@ -53,55 +53,4 @@ class MailSender
         }
     }
 
-    /**
-     * @param Event $event
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
-     * send notification to people who subscribed to the newsletter
-     */
-    public function notifyEventToMembers(Event $event)
-    {
-        $email = (new TemplatedEmail())
-            ->from($this->params->get("mailer_from"))
-            ->subject('Communication de Villereau');
-
-        $members = $this->newsletterEmailRepository->findAll();
-
-        foreach($members as $member) {
-            $email
-                ->to($member->getEmail())
-                ->htmlTemplate('email/eventNotification.html.twig')
-                ->context([
-                    'secret' => $member->getSecret(),
-                    'event' => $event,
-                ]);
-
-            $this->mailer->send($email);
-        }
-    }
-
-    /**
-     * @param Event $event
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
-     * notify event to people who have a subscriber account
-     */
-    public function notifyEventToSubscribers(Event $event, $isModified = false)
-    {
-        $email = (new TemplatedEmail())
-            ->from($this->params->get("mailer_from"))
-            ->subject('Communication de Villereau');
-
-        $members = $this->userRepository->findBy(array('isVerified' => true));
-
-        foreach($members as $member) {
-            $email
-                ->to($member->getUsername())
-                ->htmlTemplate('email/eventNotification.html.twig')
-                ->context([
-                    'event' => $event,
-                    'isModified' => $isModified
-                ]);
-
-            $this->mailer->send($email);
-        }
-    }
 }
